2b2fd8e87f25b289ea71624a09ad3b4a27d00e60
John Yarham <yarhamjohn@gmail.com>
2022-07-15 16:28:10 +0100

    Part One: Hello World

---
+++ Part One/Receive/Receive.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Receive;
+
+internal static class Receive
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory { HostName = "localhost" };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+
+            Console.WriteLine($" [x] Received {message}");
+        };
+
+        channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
---
+++ Part One/Send/Send.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+namespace Send;
+
+internal static class Send
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory { HostName = "localhost" };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+        const string message = "Hello World";
+
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+
+        Console.WriteLine($" [x] Sent {message}");
+
+        Console.WriteLine("Press [enter] to exit.");
+
+        Console.ReadLine();
+    }
+}
